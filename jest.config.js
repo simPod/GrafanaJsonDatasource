973b38138533fc1b72abe908a8923bc33a918134
Simon Podlipsky <simon@podlipsky.net>
2019-06-04 11:02:34 +0200

    Migrate Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,28 +1,25 @@
 module.exports = {
   verbose: true,
-  "globals": {
-    "ts-jest": {
-      "tsConfigFile": "tsconfig.jest.json",
-      "useBabelrc": true
-    }
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json',
+      babelConfig: true,
+    },
   },
-  "moduleNameMapper": {
-    // 'app/core/utils/datemath': '<rootDir>/node_modules/grafana-sdk-mocks/app/core/utils/datemath.ts',
-    // 'app/core/utils/kbn': '<rootDir>/src/__mocks__/kbn.ts',
+  moduleNameMapper: {
     'app/plugins/sdk': '<rootDir>/node_modules/grafana-sdk-mocks/app/plugins/sdk.ts',
   },
-  "transformIgnorePatterns": [
-    "node_modules/(?!(grafana-sdk-mocks))"
+  transformIgnorePatterns: [
+    'node_modules/(?!(grafana-sdk-mocks))',
   ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  "testRegex": "(\\.|/)([jt]est)\\.ts$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json"
-  ]
+  testRegex: '(\\.|/)([jt]est)\\.ts$',
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'ts',
+    'tsx',
+  ],
+  preset: 'ts-jest',
+  testMatch: null,
 };

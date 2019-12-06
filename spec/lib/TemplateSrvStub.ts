ba86f12d0993f9948a155a46d7154ade3ce83af8
Ivan Kopeykin <ikopeykin@ozon.ru>
2019-12-06 17:25:19 +0300

    fix(TemplateSrvStub): add link to grafana sources

--- spec/lib/TemplateSrvStub.ts
+++ spec/lib/TemplateSrvStub.ts
@@ -5,6 +5,11 @@ export default class TemplateSrvStub {
   templateSettings = { interpolate: /\[\[([\s\S]+?)\]\]/g };
   data = {};
 
+  // Original grafana source
+  // tslint:disable-next-line:max-line-length
+  // https://github.com/grafana/grafana/blob/e03d702d0c14b214a57ddd5094ff756e845cdd2b/public/app/features/templating/variable.ts#L11
+  regex = /\$(\w+)|\[\[([\s\S]+?)(?::(\w+))?\]\]|\${(\w+)(?:\.([^:^\}]+))?(?::(\w+))?}/g;
+
   replace(text) {
     return template(text, this.templateSettings)(this.data);
   }

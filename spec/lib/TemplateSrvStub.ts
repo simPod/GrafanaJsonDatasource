686897acab361ae3d7a11915249cb3600febc4ae
Simon Podlipsky <simon@podlipsky.net>
2021-07-10 19:51:38 +0200

    Refactor tests (#185)
    * Fix DataSource.jest.ts ts issues
    
    * Cleanup TemplateSrvStub.ts

--- spec/lib/TemplateSrvStub.ts
+++ spec/lib/TemplateSrvStub.ts
@@ -5,38 +5,21 @@ import template from 'lodash/template';
 export default class TemplateSrvStub implements TemplateSrv {
   templateSettings = { interpolate: /\[\[([\s\S]+?)\]\]/g };
   data = {};
-  // Original grafana source
+  adhocFilters = [];
   // tslint:disable-next-line:max-line-length
   // https://github.com/grafana/grafana/blob/e03d702d0c14b214a57ddd5094ff756e845cdd2b/public/app/features/templating/variable.ts#L11
   regex = /\$(\w+)|\[\[([\s\S]+?)(?::(\w+))?\]\]|\${(\w+)(?:\.([^:^\}]+))?(?::(\w+))?}/g;
-
   replace(target?: string, scopedVars?: ScopedVars, format?: string | Function): string {
     return template(target, this.templateSettings)(this.data);
-  }
-
-  getAdhocFilters() {
-    return [];
   }
 
   getVariables() {
     return [];
   }
 
-  variableExists() {
-    return false;
+  getAdhocFilters(datasourceName: string) {
+    return this.adhocFilters;
   }
-
-  highlightVariablesAsHtml(str) {
-    return str;
-  }
-
-  setGrafanaVariable(name, value) {
-    this.data[name] = value;
-  }
-
-  init() {}
-
-  fillVariableValuesForUrl() {}
 
   updateTemplateData() {}
 }

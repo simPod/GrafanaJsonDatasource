d917c7da3d82998e7eb627a2f72a582eb7eb0f4b
Simon Podlipsky <simon@podlipsky.net>
2021-07-10 18:54:35 +0200

    Override @emotion/core type definition (missing css prop) as @grafana/ui v8.0.2 still requires it

--- src/types.d.ts
+++ src/types.d.ts
@@ -53,3 +53,9 @@ export interface MultiValueVariable extends VariableModel {
   current: TextValuePair;
   options: TextValuePair[];
 }
+
+declare module 'react' {
+  interface DOMAttributes<T> {
+    css?: InterpolationWithTheme<any>;
+  }
+}

19066b4738e9aa2ebc60abc420b140fe03f99a68
Simon Podlipsky <simon@podlipsky.net>
2022-04-20 11:11:07 +0200

    Drop old annotations (#252)

--- src/response_parser.ts
+++ src/response_parser.ts
@@ -1,5 +1,5 @@
-import { AnnotationEvent, DataFrame, MetricFindValue, toDataFrame } from '@grafana/data';
-import { BackendDataSourceResponse, FetchResponse, toDataQueryResponse } from '@grafana/runtime';
+import { MetricFindValue, toDataFrame } from '@grafana/data';
+import { BackendDataSourceResponse, FetchResponse } from '@grafana/runtime';
 
 // https://github.com/grafana/grafana/blob/560c77390550e12e8c0be507c00f27cde0aa31e5/public/app/plugins/datasource/postgres/response_parser.ts
 export class ResponseParser {
@@ -28,42 +28,5 @@ export class ResponseParser {
       text,
       value: values.find((v) => v.text === text)?.value,
     }));
-  }
-
-  async transformAnnotationResponse(options: any, data: BackendDataSourceResponse): Promise<AnnotationEvent[]> {
-    const frames = toDataQueryResponse({ data: data }).data as DataFrame[];
-    if (!frames || !frames.length) {
-      return [];
-    }
-    const frame = frames[0];
-    const timeField = frame.fields.find((f) => f.name === 'time');
-
-    if (!timeField) {
-      throw new Error('Missing mandatory time column (with time column alias) in annotation query');
-    }
-
-    const timeEndField = frame.fields.find((f) => f.name === 'timeend');
-    const textField = frame.fields.find((f) => f.name === 'text');
-    const tagsField = frame.fields.find((f) => f.name === 'tags');
-
-    const list: AnnotationEvent[] = [];
-    for (let i = 0; i < frame.length; i++) {
-      const timeEnd = timeEndField && timeEndField.values.get(i) ? Math.floor(timeEndField.values.get(i)) : undefined;
-      list.push({
-        annotation: options.annotation,
-        time: Math.floor(timeField.values.get(i)),
-        timeEnd,
-        text: textField && textField.values.get(i) ? textField.values.get(i) : '',
-        tags:
-          tagsField && tagsField.values.get(i)
-            ? tagsField.values
-                .get(i)
-                .trim()
-                .split(/\s*,\s*/)
-            : [],
-      });
-    }
-
-    return list;
   }
 }

e376a29ae8aa60f99964bf812d2deddd974df347
Simon Podlipsky <simon@podlipsky.net>
2022-10-28 15:43:19 +0200

    test: use await in tests so jest does not timeout with no reason (#331)

--- spec/DataSource.jest.ts
+++ spec/DataSource.jest.ts
@@ -1,5 +1,5 @@
-import { isDataFrame } from '@grafana/data';
-import { DateTime } from '@grafana/data/datetime/moment_wrapper';
+import {isDataFrame} from '@grafana/data';
+import {DateTime} from '@grafana/data/datetime/moment_wrapper';
 import {
   BackendSrv,
   FetchResponse,
@@ -8,9 +8,9 @@ import {
   setBackendSrv,
   setTemplateSrv,
 } from '@grafana/runtime';
-import { of } from 'rxjs';
-import { DataSource } from '../src/DataSource';
-import { QueryRequest } from '../src/types';
+import {of} from 'rxjs';
+import {DataSource} from '../src/DataSource';
+import {QueryRequest} from '../src/types';
 import TemplateSrvStub from './lib/TemplateSrvStub';
 
 const options = {
@@ -51,28 +51,14 @@ const options = {
 describe('GenericDatasource', () => {
   const ds = new DataSource({} as any);
 
-  it('should return an empty array when no targets are set', (done) => {
-    ds.query({ ...options, targets: [] }).then((result) => {
-      expect(result.data).toHaveLength(0);
-      done();
-    });
+  it('should return an empty array when no targets are set', async () => {
+    const result = await ds.query({...options, targets: []});
+
+    expect(result.data).toHaveLength(0);
   });
 
-  it('should return the server results when a target is set', (done) => {
+  it('should return the server results when a target is set', async () => {
     setBackendSrv({
-      fetch: (request) =>
-        of({
-          data: [
-            {
-              target: 'X',
-              datapoints: [
-                [1, 1621077300000],
-                [2, 1621077600000],
-                [3, 1621077900000],
-              ],
-            },
-          ],
-        }),
       fetch: (request) =>
         of({
           data: [
@@ -92,17 +78,92 @@ describe('GenericDatasource', () => {
     templateSrvStub.replace = (data) => data ?? '';
     setTemplateSrv(templateSrvStub);
 
-    ds.query({ ...options, targets: [{ refId: 'A', payload: '', target: 'hits' }] }).then((result) => {
-      const series = result.data[0];
-      expect(isDataFrame(series)).toBe(true);
-      expect(series.name).toBe('X');
-      expect(series.length).toBe(3);
-      expect(series.fields[0].values).toHaveLength(3);
-      done();
-    });
+    const result = await ds.query({...options, targets: [{refId: 'A', payload: '', target: 'hits'}]});
+
+    const series = result.data[0];
+    expect(isDataFrame(series)).toBe(true);
+    expect(series.name).toBe('X');
+    expect(series.length).toBe(3);
+    expect(series.fields[0].values).toHaveLength(3);
   });
 
-  it('should return the metric target results when a target is set', (done) => {
+  it('should return the metric target results when a target is set', async () => {
+    setBackendSrv({
+      fetch: (request) => {
+        const target = request.data.target;
+        const result = [target + '_0', target + '_1', target + '_2'];
+
+        return of({
+          _request: request,
+          data: result,
+        } as unknown as FetchResponse);
+      }
+    } as BackendSrv);
+
+    const templateSrvStub = new TemplateSrvStub();
+    templateSrvStub.replace = (data) => data ?? '';
+    setTemplateSrv(templateSrvStub);
+
+    const result = await ds.listMetrics('search');
+
+    expect(result).toHaveLength(3);
+    expect(result[0].text).toBe('search_0');
+    expect(result[0].value).toBe('search_0');
+    expect(result[1].text).toBe('search_1');
+    expect(result[1].value).toBe('search_1');
+    expect(result[2].text).toBe('search_2');
+    expect(result[2].value).toBe('search_2');
+  });
+
+  it('should return the metric results when the target is an empty string', async () => {
+    setBackendSrv({
+      fetch: (request) =>
+        of({
+          _request: request,
+          data: ['metric_0', 'metric_1', 'metric_2'],
+        } as unknown as FetchResponse)
+    } as BackendSrv);
+
+    const templateSrvStub = new TemplateSrvStub();
+    templateSrvStub.replace = (data) => data ?? '';
+    setTemplateSrv(templateSrvStub);
+
+    const result = await ds.listMetrics('');
+
+    expect(result).toHaveLength(3);
+    expect(result[0].text).toBe('metric_0');
+    expect(result[0].value).toBe('metric_0');
+    expect(result[1].text).toBe('metric_1');
+    expect(result[1].value).toBe('metric_1');
+    expect(result[2].text).toBe('metric_2');
+    expect(result[2].value).toBe('metric_2');
+  });
+
+  it('should return the metric results when the args are an empty object', async () => {
+    setBackendSrv({
+      fetch: (request) =>
+        of({
+          _request: request,
+          data: ['metric_0', 'metric_1', 'metric_2'],
+        } as unknown as FetchResponse)
+    } as BackendSrv);
+
+    const templateSrvStub = new TemplateSrvStub();
+    templateSrvStub.replace = (data) => data ?? '';
+    setTemplateSrv(templateSrvStub);
+
+    const result = await ds.listMetrics('');
+
+    expect(result).toHaveLength(3);
+    expect(result[0].text).toBe('metric_0');
+    expect(result[0].value).toBe('metric_0');
+    expect(result[1].text).toBe('metric_1');
+    expect(result[1].value).toBe('metric_1');
+    expect(result[2].text).toBe('metric_2');
+    expect(result[2].value).toBe('metric_2');
+  });
+
+  it('should return the metric target results when the args are a string', async () => {
     getBackendSrv().fetch = (request) => {
       const target = request.data.target;
       const result = [target + '_0', target + '_1', target + '_2'];
@@ -117,92 +178,18 @@ describe('GenericDatasource', () => {
     templateSrvStub.replace = (data) => data ?? '';
     setTemplateSrv(templateSrvStub);
 
-    ds.listMetrics('search').then((result) => {
-      expect(result).toHaveLength(3);
-      expect(result[0].text).toBe('search_0');
-      expect(result[0].value).toBe('search_0');
-      expect(result[1].text).toBe('search_1');
-      expect(result[1].value).toBe('search_1');
-      expect(result[2].text).toBe('search_2');
-      expect(result[2].value).toBe('search_2');
-      done();
-    });
+    const result = await ds.listMetrics('search');
+
+    expect(result).toHaveLength(3);
+    expect(result[0].text).toBe('search_0');
+    expect(result[0].value).toBe('search_0');
+    expect(result[1].text).toBe('search_1');
+    expect(result[1].value).toBe('search_1');
+    expect(result[2].text).toBe('search_2');
+    expect(result[2].value).toBe('search_2');
   });
 
-  it('should return the metric results when the target is an empty string', (done) => {
-    getBackendSrv().fetch = (request) =>
-      of({
-        _request: request,
-        data: ['metric_0', 'metric_1', 'metric_2'],
-      } as unknown as FetchResponse);
-
-    const templateSrvStub = new TemplateSrvStub();
-    templateSrvStub.replace = (data) => data ?? '';
-    setTemplateSrv(templateSrvStub);
-
-    ds.listMetrics('').then((result) => {
-      expect(result).toHaveLength(3);
-      expect(result[0].text).toBe('metric_0');
-      expect(result[0].value).toBe('metric_0');
-      expect(result[1].text).toBe('metric_1');
-      expect(result[1].value).toBe('metric_1');
-      expect(result[2].text).toBe('metric_2');
-      expect(result[2].value).toBe('metric_2');
-      done();
-    });
-  });
-
-  it('should return the metric results when the args are an empty object', (done) => {
-    getBackendSrv().fetch = (request) =>
-      of({
-        _request: request,
-        data: ['metric_0', 'metric_1', 'metric_2'],
-      } as unknown as FetchResponse);
-
-    const templateSrvStub = new TemplateSrvStub();
-    templateSrvStub.replace = (data) => data ?? '';
-    setTemplateSrv(templateSrvStub);
-
-    ds.listMetrics('').then((result) => {
-      expect(result).toHaveLength(3);
-      expect(result[0].text).toBe('metric_0');
-      expect(result[0].value).toBe('metric_0');
-      expect(result[1].text).toBe('metric_1');
-      expect(result[1].value).toBe('metric_1');
-      expect(result[2].text).toBe('metric_2');
-      expect(result[2].value).toBe('metric_2');
-      done();
-    });
-  });
-
-  it('should return the metric target results when the args are a string', (done) => {
-    getBackendSrv().fetch = (request) => {
-      const target = request.data.target;
-      const result = [target + '_0', target + '_1', target + '_2'];
-
-      return of({
-        _request: request,
-        data: result,
-      } as unknown as FetchResponse);
-    };
-
-    const templateSrvStub = new TemplateSrvStub();
-    templateSrvStub.replace = (data) => data ?? '';
-    setTemplateSrv(templateSrvStub);
-
-    ds.listMetrics('search').then((result) => {
-      expect(result).toHaveLength(3);
-      expect(result[0].text).toBe('search_0');
-      expect(result[0].value).toBe('search_0');
-      expect(result[1].text).toBe('search_1');
-      expect(result[1].value).toBe('search_1');
-      expect(result[2].text).toBe('search_2');
-      expect(result[2].value).toBe('search_2');
-      done();
-    });
-  });
-
-  it('should accept raw json instead of a target/string query for template vars', (done) => {
+  it('should accept raw json instead of a target/string query for template vars', async () => {
     let jsonParsed = false;
     getBackendSrv().fetch = (request) => {
       const payload = request.data.payload;
@@ -220,14 +207,13 @@ describe('GenericDatasource', () => {
     templateSrvStub.replace = (data) => data ?? '';
     setTemplateSrv(templateSrvStub);
 
-    ds.metricFindQuery({ query: `{"target":"search"}`, format: 'json' }).then((result) => {
-      expect(jsonParsed).toBe(true);
-      done();
-    });
+    await ds.metricFindQuery({query: `{"target":"search"}`, format: 'json'});
+
+    expect(jsonParsed).toBe(true);
   });
 
   it('should return data as text and as value', (done) => {
-    const result = ds.mapToTextValue({ data: ['zero', 'one', 'two'] });
+    const result = ds.mapToTextValue({data: ['zero', 'one', 'two']});
 
     expect(result).toHaveLength(3);
     expect(result[0].text).toBe('zero');
@@ -241,12 +227,12 @@ describe('GenericDatasource', () => {
 
   it('should return text as text and value as value', (done) => {
     const data = [
-      { text: 'zero', value: 'value_0' },
-      { text: 'one', value: 'value_1' },
-      { text: 'two', value: 'value_2' },
+      {text: 'zero', value: 'value_0'},
+      {text: 'one', value: 'value_1'},
+      {text: 'two', value: 'value_2'},
     ];
 
-    const result = ds.mapToTextValue({ data });
+    const result = ds.mapToTextValue({data});
 
     expect(result).toHaveLength(3);
     expect(result[0].text).toBe('zero');
@@ -260,12 +246,12 @@ describe('GenericDatasource', () => {
 
   it('should return data as text and index as value', (done) => {
     const data = [
-      { a: 'zero', b: 'value_0' },
-      { a: 'one', b: 'value_1' },
-      { a: 'two', b: 'value_2' },
+      {a: 'zero', b: 'value_0'},
+      {a: 'one', b: 'value_1'},
+      {a: 'two', b: 'value_2'},
     ];
 
-    const result = ds.mapToTextValue({ data });
+    const result = ds.mapToTextValue({data});
 
     expect(result).toHaveLength(3);
     expect(result[0].text).toBe(data[0]);
@@ -277,10 +263,10 @@ describe('GenericDatasource', () => {
     done();
   });
 
-  it('should support tag keys', (done) => {
+  it('should support tag keys', async () => {
     const data = [
-      { type: 'string', text: 'One', key: 'one' },
-      { type: 'string', text: 'two', key: 'Two' },
+      {type: 'string', text: 'One', key: 'one'},
+      {type: 'string', text: 'two', key: 'Two'},
     ];
 
     getBackendSrv().fetch = (request) =>
@@ -289,41 +275,41 @@ describe('GenericDatasource', () => {
         _request: request,
       } as unknown as FetchResponse);
 
-    ds.getTagKeys().then((result) => {
-      expect(result).toHaveLength(2);
-      expect(result[0].type).toBe(data[0].type);
-      expect(result[0].text).toBe(data[0].text);
-      expect(result[0].key).toBe(data[0].key);
-      expect(result[1].type).toBe(data[1].type);
-      expect(result[1].text).toBe(data[1].text);
-      expect(result[1].key).toBe(data[1].key);
-      done();
-    });
+    const result = await ds.getTagKeys();
+
+    expect(result).toHaveLength(2);
+    expect(result[0].type).toBe(data[0].type);
+    expect(result[0].text).toBe(data[0].text);
+    expect(result[0].key).toBe(data[0].key);
+    expect(result[1].type).toBe(data[1].type);
+    expect(result[1].text).toBe(data[1].text);
+    expect(result[1].key).toBe(data[1].key);
   });
 
-  it('should support tag values', (done) => {
+  it('should support tag values', async () => {
     const data = [
-      { key: 'eins', text: 'Eins!' },
-      { key: 'zwei', text: 'Zwei' },
-      { key: 'drei', text: 'Drei!' },
+      {key: 'eins', text: 'Eins!'},
+      {key: 'zwei', text: 'Zwei'},
+      {key: 'drei', text: 'Drei!'},
     ];
 
-    getBackendSrv().fetch = (request) =>
-      of({
-        data,
-        _request: request,
-      } as unknown as FetchResponse);
+    setBackendSrv({
+      fetch: (request) =>
+        of({
+          data,
+          _request: request,
+        } as unknown as FetchResponse)
+    } as BackendSrv);
 
-    ds.getTagValues(null).then((result) => {
-      expect(result).toHaveLength(3);
-      expect(result[0].text).toBe(data[0].text);
-      expect(result[0].key).toBe(data[0].key);
-      expect(result[1].text).toBe(data[1].text);
-      expect(result[1].key).toBe(data[1].key);
-      expect(result[2].text).toBe(data[2].text);
-      expect(result[2].key).toBe(data[2].key);
-      done();
-    });
+    const result = await ds.getTagValues(null);
+
+    expect(result).toHaveLength(3);
+    expect(result[0].text).toBe(data[0].text);
+    expect(result[0].key).toBe(data[0].key);
+    expect(result[1].text).toBe(data[1].text);
+    expect(result[1].key).toBe(data[1].key);
+    expect(result[2].text).toBe(data[2].text);
+    expect(result[2].key).toBe(data[2].key);
   });
 });
 
@@ -357,7 +343,7 @@ describe('GenericDatasource.prototype.buildQueryTargets', () => {
 
     expect(ds.processTargets(testcase).targets).toMatchObject([
       {
-        payload: { A: REPLACED_VALUE },
+        payload: {A: REPLACED_VALUE},
         target: testcase.targets[0].target,
         refId: testcase.targets[0].refId,
         hide: testcase.targets[0].hide,
@@ -388,8 +374,8 @@ describe('GenericDatasource.prototype.buildQueryTargets', () => {
       {
         payload: {
           filters: [
-            { key: 'SOME', value: REPLACED_VALUE },
-            { key: 'SOME2', value: REPLACED_VALUE },
+            {key: 'SOME', value: REPLACED_VALUE},
+            {key: 'SOME2', value: REPLACED_VALUE},
           ],
         },
         target: testcase.targets[0].target,
@@ -420,7 +406,7 @@ describe('GenericDatasource.prototype.buildQueryTargets', () => {
     expect(ds.processTargets(testcase).targets).toMatchObject([
       {
         payload: {
-          filters: [{ A: `${REPLACED_VALUE} ms` }],
+          filters: [{A: `${REPLACED_VALUE} ms`}],
         },
         target: testcase.targets[0].target,
         refId: testcase.targets[0].refId,
@@ -465,8 +451,8 @@ describe('GenericDatasource.prototype.buildQueryTargets', () => {
       {
         payload: {
           filters: [
-            { key: 'SOME', value: REPLACED_NUMBER_VALUE },
-            { key: 'SOME2', value: REPLACED_NUMBER_VALUE },
+            {key: 'SOME', value: REPLACED_NUMBER_VALUE},
+            {key: 'SOME2', value: REPLACED_NUMBER_VALUE},
           ],
         },
         target: testcase.targets[0].target,
@@ -512,8 +498,8 @@ describe('GenericDatasource.prototype.buildQueryTargets', () => {
       {
         payload: {
           filters: [
-            { key: 'SOME', value: REPLACED_BOOLEAN_VALUE },
-            { key: 'SOME2', value: REPLACED_BOOLEAN_VALUE },
+            {key: 'SOME', value: REPLACED_BOOLEAN_VALUE},
+            {key: 'SOME2', value: REPLACED_BOOLEAN_VALUE},
           ],
         },
         target: testcase.targets[0].target,

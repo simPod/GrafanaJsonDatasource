e4cad9a59cd4200a2b0a46dbcd855c549cc46d2c
Simon Podlipsky <simon@podlipsky.net>
2024-06-26 12:39:37 +0300

    feat: use VariableSupport

--- src/module.ts
+++ src/module.ts
@@ -3,9 +3,7 @@ import { ConfigEditor } from './Component/ConfigEditor';
 import { QueryEditorContainer } from './Component/QueryEditorContainer';
 import { DataSource } from './DataSource';
 import { GenericOptions, GrafanaQuery } from './types';
-import { VariableQueryEditor } from './Component/VariableQueryEditor';
 
 export const plugin = new DataSourcePlugin<DataSource, GrafanaQuery, GenericOptions>(DataSource)
   .setConfigEditor(ConfigEditor)
-  .setQueryEditor(QueryEditorContainer)
-  .setVariableQueryEditor(VariableQueryEditor);
+  .setQueryEditor(QueryEditorContainer);
